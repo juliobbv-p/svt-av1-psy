a85f1bf46602576e1eeb2db8550c279ae39a6b3e
Gianni Rosato <giannirosato@proton.me>
2024-02-18 02:57:17 -0500

    feat: CLI prettiness

--- Source/App/app_config.c
+++ Source/App/app_config.c
@@ -1785,7 +1785,7 @@ static bool check_long(const ConfigDescription* cfg_entry, const ConfigDescripti
 }
 
 static void print_options(const char* title, const ConfigDescription* options) {
-    printf("\n%s:\n", title);
+    printf("\n\x1b[1;4m%s\x1b[0m:\n", title);
 
     for (const ConfigDescription* index = options; index->token; ++index) {
         // this only works if short and long token are one after another
@@ -1828,9 +1828,9 @@ uint32_t get_help(int32_t argc, char* const argv[]) {
     }
 
     printf(
-        "Usage: SvtAv1EncApp <options> <-b dst_filename> -i src_filename\n"
+        "\x1b[1;4mUsage\x1b[0m: SvtAv1EncApp <options> <-b dst_filename> -i src_filename\n"
         "\n"
-        "Examples:\n"
+        "\x1b[1;4mExamples\x1b[0m:\n"
         "Multi-pass encode (VBR):\n"
         "    SvtAv1EncApp <--stats svtav1_2pass.log> --passes 2 --rc 1 --tbr 1000 -b dst_filename "
         "-i src_filename\n"
