Uranite/svt-av1-tritium

Add --alt-ssim-tuning for psychovisual enhancements to tune 2

Source/Lib/Codec/src_ops_process.c
@@ -2123,6 +2123,63 @@ unsigned int svt_aom_get_perpixel_variance(const uint8_t *buf, uint32_t stride, 
     var                            = fn_ptr->vf(buf, stride, AV1_VAR_OFFS, 0, &sse);
     return ROUND_POWER_OF_TWO(var, eb_num_pels_log2_lookup[block_size]);
 }
+void svt_aom_get_mean_and_perpixel_variance(const uint8_t *buf, uint32_t stride, const int block_size,
+                                            uint32_t *perpixel_var, uint32_t *mean) {
+    const int block_w = block_size_wide[block_size];
+    const int block_h = block_size_high[block_size];
+
+    uint64_t sum = 0;
+    uint64_t sse = 0;
+
+    // First pass: calculate sum
+    for (int r = 0; r < block_h; ++r) {
+        const uint8_t *row = buf + r * stride;
+        for (int c = 0; c < block_w; ++c) { sum += row[c]; }
+    }
+    *mean = (uint32_t)ROUND_POWER_OF_TWO(sum, eb_num_pels_log2_lookup[block_size]);
+
+    // Second pass: calculate sum of squared errors
+    for (int r = 0; r < block_h; ++r) {
+        for (int c = 0; c < block_w; ++c) {
+            const int diff = buf[r * stride + c] - *mean;
+            sse += diff * diff;
+        }
+    }
+
+    *perpixel_var = (uint32_t)ROUND_POWER_OF_TWO(sse, eb_num_pels_log2_lookup[block_size]);
+}
+unsigned int svt_aom_get_perceptual_perpixel_variance(const uint8_t *buf, uint32_t stride, const int block_size) {
+    unsigned int var, mean;
+
+    // In a real implementation, you would use an optimized AVX2 function
+    // that calculates mean and variance in one go. For this example, we use our helper.
+    // The existing svt_aom_mefn_ptr->vf can't be used as it doesn't expose the mean.
+    svt_aom_get_mean_and_perpixel_variance(buf, stride, block_size, &var, &mean);
+
+    // HVS is most sensitive around mid-grays (e.g., 110-140 for 8-bit video).
+    // We create a weight that is highest in this range and falls off towards black and white.
+    // A simple parabolic function centered at 128 is a good model.
+    // weight = 1.0 - (|mean - 128| / 128)^2
+    // We can use integer arithmetic for speed.
+    int centered_mean    = (int)mean - 128;
+    int weight_numerator = 128 * 128 - centered_mean * centered_mean; // (128 - |mean-128|) * (128 + |mean-128|)
+
+    // Scale the weight to avoid floating point math. e.g., 256 is 1.0x
+    // The final weight will be in [0, 256].
+    int weight = (weight_numerator * 256) / (128 * 128);
+
+    // Let's invert the weight's effect. We want to increase the variance
+    // value for mid-tones, making the encoder think they are more complex and
+    // deserving of bits. A simple way is to scale by a factor > 1.0.
+    // A better perceptual weight boosts the importance of mid-tones.
+    // Let's define a boost factor, e.g., boosting it by up to 50%, or use another equation (var + var*weight / sqrt(var + 1))
+    //unsigned int perceptual_var = var * (1.0 + weight);
+    unsigned int perceptual_var = var + ((var * weight) / sqrtf(var + 1.));
+
+    //fprintf(stdout, "%.4u\t", perceptual_var);
+
+    return perceptual_var;
+}
 static void aom_av1_set_mb_ssim_rdmult_scaling(PictureParentControlSet *pcs) {
     if (!pcs->scs->static_config.enable_tpl_la) // tuning rdmult with SSIM requires TPL ME data
         return;
@@ -2156,59 +2213,132 @@ static void aom_av1_set_mb_ssim_rdmult_scaling(PictureParentControlSet *pcs) {
             double    var = 0.0, num_of_var = 0.0;
             const int index = row * num_cols + col;
 
-            // Loop through each 8x8 block.
-            for (int mi_row = row * num_mi_h; mi_row < cm->mi_rows && mi_row < (row + 1) * num_mi_h; mi_row += 2) {
-                for (int mi_col = col * num_mi_w; mi_col < cm->mi_cols && mi_col < (col + 1) * num_mi_w; mi_col += 2) {
-                    const int row_offset_y = mi_row << 2;
-                    const int col_offset_y = mi_col << 2;
+            if (pcs->scs->static_config.alt_ssim_tuning) {
+                const int mi_row       = row << 2;
+                const int mi_col       = col << 2;
+                const int row_offset_y = row << 2;
+                const int col_offset_y = col << 2;
 
-                    const uint8_t *buf = y_buffer + row_offset_y * y_stride + col_offset_y;
+                // Loop through each 4x4 block within the 16x16 block.
+                for (int mi_row_3 = mi_row; mi_row_3 < cm->mi_rows && mi_row_3 < (row + 1) * num_mi_h; mi_row_3 += 1) {
+                    for (int mi_col_3 = mi_col; mi_col_3 < cm->mi_cols && mi_col_3 < (col + 1) * num_mi_w;
+                         mi_col_3 += 1) {
+                        const int row_offset_y_3 = mi_row_3 << 2;
+                        const int col_offset_y_3 = mi_col_3 << 2;
 
-                    var += svt_aom_get_perpixel_variance(buf, y_stride, BLOCK_8X8);
-                    num_of_var += 1.0;
+                        const uint8_t *buf3 = y_buffer + row_offset_y_3 * y_stride + col_offset_y_3;
+
+                        var += svt_aom_get_perceptual_perpixel_variance(buf3, y_stride, BLOCK_4X4);
+                        num_of_var += 0.015625; // Weigh at 1x weight (0.25 of total num_of_var)
+                    }
                 }
-            }
-            var = var / num_of_var;
+                // Loop through each 8x8 block within the 16x16 block.
+                for (int mi_row_2 = mi_row; mi_row_2 < cm->mi_rows && mi_row_2 < (row + 1) * num_mi_h; mi_row_2 += 2) {
+                    for (int mi_col_2 = mi_col; mi_col_2 < cm->mi_cols && mi_col_2 < (col + 1) * num_mi_w;
+                         mi_col_2 += 2) {
+                        const int row_offset_y_2 = mi_row_2 << 2;
+                        const int col_offset_y_2 = mi_col_2 << 2;
 
-            // Curve fitting with an exponential model on all 16x16 blocks from the
-            // midres dataset.
-            double var_backup                                   = var;
-            var                                                 = factor_a * (1 - exp(factor_b * var)) + factor_c;
-            pcs->pa_me_data->ssim_rdmult_scaling_factors[index] = var;
-            log_sum += log(var);
-            if (do_print) {
-                if (col == 0) {
-                    fprintf(stdout, "\n");
+                        const uint8_t *buf2 = y_buffer + row_offset_y_2 * y_stride + col_offset_y_2;
+
+                        var += svt_aom_get_perceptual_perpixel_variance(buf2, y_stride, BLOCK_8X8);
+                        num_of_var +=
+                            0.125; // This weight (8x8 block) is 2x more important compared to other num_of_var additions
+                        // (0.5 of total num_of_var)
+                    }
                 }
-                fprintf(stdout, "%.4f\t", var_backup);
+                const uint8_t *buf = y_buffer + row_offset_y * y_stride + col_offset_y;
+
+                var += svt_aom_get_perceptual_perpixel_variance(buf, y_stride, BLOCK_16X16);
+                num_of_var += 0.25; // Weigh at 1x weight (0.25 of total num_of_var)
+            } else {
+                // Loop through each 8x8 block.
+                for (int mi_row = row * num_mi_h; mi_row < cm->mi_rows && mi_row < (row + 1) * num_mi_h; mi_row += 2) {
+                    for (int mi_col = col * num_mi_w; mi_col < cm->mi_cols && mi_col < (col + 1) * num_mi_w;
+                         mi_col += 2) {
+                        const int row_offset_y = mi_row << 2;
+                        const int col_offset_y = mi_col << 2;
+
+                        const uint8_t *buf = y_buffer + row_offset_y * y_stride + col_offset_y;
+
+                        var += svt_aom_get_perpixel_variance(buf, y_stride, BLOCK_8X8);
+                        num_of_var += 1.0;
+                    }
+                }
+                var = var / num_of_var;
+
+                // Curve fitting with an exponential model on all 16x16 blocks from the
+                // midres dataset.
+                double var_backup                                   = var;
+                var                                                 = factor_a * (1 - exp(factor_b * var)) + factor_c;
+                pcs->pa_me_data->ssim_rdmult_scaling_factors[index] = var;
+                log_sum += log(var);
+                if (do_print) {
+                    if (col == 0) {
+                        fprintf(stdout, "\n");
+                    }
+                    fprintf(stdout, "%.4f\t", var_backup);
+                }
             }
         }
     }
-    log_sum = exp(log_sum / (double)(num_rows * num_cols));
-    if (do_print) {
-        fprintf(stdout, "\nlog_sum %.4f\n", log_sum);
-    }
+    if (!pcs->scs->static_config.alt_ssim_tuning) {
+        log_sum = exp(log_sum / (double)(num_rows * num_cols));
+        if (do_print) {
+            fprintf(stdout, "\nlog_sum %.4f\n", log_sum);
+        }
 
-    if (do_print) {
-        fprintf(stdout, "16x16 block rdmult scaling factors");
-    }
-    double min = 0xfffffff;
-    double max = 0;
-    for (int row = 0; row < num_rows; ++row) {
-        for (int col = 0; col < num_cols; ++col) {
-            const int index = row * num_cols + col;
-            pcs->pa_me_data->ssim_rdmult_scaling_factors[index] /= log_sum;
-            if (pcs->pa_me_data->ssim_rdmult_scaling_factors[index] < min) {
-                min = pcs->pa_me_data->ssim_rdmult_scaling_factors[index];
-            }
-            if (pcs->pa_me_data->ssim_rdmult_scaling_factors[index] > max) {
-                max = pcs->pa_me_data->ssim_rdmult_scaling_factors[index];
-            }
-            if (do_print) {
-                if (col == 0) {
-                    fprintf(stdout, "\n");
+        if (do_print) {
+            fprintf(stdout, "16x16 block rdmult scaling factors");
+        }
+        double min = 0xfffffff;
+        double max = 0;
+        for (int row = 0; row < num_rows; ++row) {
+            for (int col = 0; col < num_cols; ++col) {
+                const int index = row * num_cols + col;
+                pcs->pa_me_data->ssim_rdmult_scaling_factors[index] /= log_sum;
+                if (pcs->pa_me_data->ssim_rdmult_scaling_factors[index] < min) {
+                    min = pcs->pa_me_data->ssim_rdmult_scaling_factors[index];
                 }
-                fprintf(stdout, "%.4f\t", pcs->pa_me_data->ssim_rdmult_scaling_factors[index]);
+                if (pcs->pa_me_data->ssim_rdmult_scaling_factors[index] > max) {
+                    max = pcs->pa_me_data->ssim_rdmult_scaling_factors[index];
+                }
+                if (do_print) {
+                    if (col == 0) {
+                        fprintf(stdout, "\n");
+                    }
+                    fprintf(stdout, "%.4f\t", pcs->pa_me_data->ssim_rdmult_scaling_factors[index]);
+                }
+            }
+        }
+    } else { // Do superblock-based adjustment if we're using alternative SSIM tuning
+        const int sb_size     = pcs->scs->seq_header.sb_size;
+        const int num_mi_w_sb = mi_size_wide[sb_size];
+        const int num_mi_h_sb = mi_size_high[sb_size];
+        const int num_cols_sb = (cm->mi_cols + num_mi_w_sb - 1) / num_mi_w_sb;
+        const int num_rows_sb = (cm->mi_rows + num_mi_h_sb - 1) / num_mi_h_sb;
+        const int num_blk_w   = num_mi_w_sb / num_mi_w;
+        const int num_blk_h   = num_mi_h_sb / num_mi_h;
+        for (int row = 0; row < num_rows_sb; ++row) {
+            for (int col = 0; col < num_cols_sb; ++col) {
+                double log_sum_sb = 0.0;
+                double blk_count  = 0.0;
+                for (int blk_row = row * num_blk_h; blk_row < (row + 1) * num_blk_h && blk_row < num_rows; ++blk_row) {
+                    for (int blk_col = col * num_blk_w; blk_col < (col + 1) * num_blk_w && blk_col < num_cols;
+                         ++blk_col) {
+                        const int index = blk_row * num_cols + blk_col;
+                        log_sum_sb += log(pcs->pa_me_data->ssim_rdmult_scaling_factors[index]);
+                        blk_count += 1.0;
+                    }
+                }
+                log_sum_sb = exp(log_sum_sb / blk_count);
+                for (int blk_row = row * num_blk_h; blk_row < (row + 1) * num_blk_h && blk_row < num_rows; ++blk_row) {
+                    for (int blk_col = col * num_blk_w; blk_col < (col + 1) * num_blk_w && blk_col < num_cols;
+                         ++blk_col) {
+                        const int index = blk_row * num_cols + blk_col;
+                        pcs->pa_me_data->ssim_rdmult_scaling_factors[index] /= log_sum_sb;
+                    }
+                }
             }
         }
     }

juliobbv-p/svt-av1-psy

Source/Lib/Codec/src_ops_process.c
@@ -2145,6 +2145,67 @@ unsigned int svt_aom_get_perpixel_variance(const uint8_t* buf, uint32_t stride, 
     return ROUND_POWER_OF_TWO(var, eb_num_pels_log2_lookup[block_size]);
 }
 
+void svt_aom_get_mean_and_perpixel_variance(const uint8_t* buf, uint32_t stride, const int block_size,
+                                            uint32_t* perpixel_var, uint32_t* mean) {
+    const int block_w = block_size_wide[block_size];
+    const int block_h = block_size_high[block_size];
+
+    uint64_t sum = 0;
+    uint64_t sse = 0;
+
+    // First pass: calculate sum
+    for (int r = 0; r < block_h; ++r) {
+        const uint8_t* row = buf + r * stride;
+        for (int c = 0; c < block_w; ++c) {
+            sum += row[c];
+        }
+    }
+    *mean = (uint32_t)ROUND_POWER_OF_TWO(sum, eb_num_pels_log2_lookup[block_size]);
+
+    // Second pass: calculate sum of squared errors
+    for (int r = 0; r < block_h; ++r) {
+        for (int c = 0; c < block_w; ++c) {
+            const int diff = buf[r * stride + c] - *mean;
+            sse += diff * diff;
+        }
+    }
+
+    *perpixel_var = (uint32_t)ROUND_POWER_OF_TWO(sse, eb_num_pels_log2_lookup[block_size]);
+}
+
+unsigned int svt_aom_get_perceptual_perpixel_variance(const uint8_t* buf, uint32_t stride, const int block_size) {
+    unsigned int var, mean;
+
+    // In a real implementation, you would use an optimized AVX2 function
+    // that calculates mean and variance in one go. For this example, we use our helper.
+    // The existing svt_aom_mefn_ptr->vf can't be used as it doesn't expose the mean.
+    svt_aom_get_mean_and_perpixel_variance(buf, stride, block_size, &var, &mean);
+
+    // HVS is most sensitive around mid-grays (e.g., 110-140 for 8-bit video).
+    // We create a weight that is highest in this range and falls off towards black and white.
+    // A simple parabolic function centered at 128 is a good model.
+    // weight = 1.0 - (|mean - 128| / 128)^2
+    // We can use integer arithmetic for speed.
+    int centered_mean    = (int)mean - 128;
+    int weight_numerator = 128 * 128 - centered_mean * centered_mean; // (128 - |mean-128|) * (128 + |mean-128|)
+
+    // Scale the weight to avoid floating point math. e.g., 256 is 1.0x
+    // The final weight will be in [0, 256].
+    int weight = (weight_numerator * 256) / (128 * 128);
+
+    // Let's invert the weight's effect. We want to increase the variance
+    // value for mid-tones, making the encoder think they are more complex and
+    // deserving of bits. A simple way is to scale by a factor > 1.0.
+    // A better perceptual weight boosts the importance of mid-tones.
+    // Let's define a boost factor, e.g., boosting it by up to 50%, or use another equation (var + var*weight / sqrt(var + 1))
+    //unsigned int perceptual_var = var * (1.0 + weight);
+    unsigned int perceptual_var = var + ((var * weight) / sqrtf(var + 1.));
+
+    //fprintf(stdout, "%.4u\t", perceptual_var);
+
+    return perceptual_var;
+}
+
 static void aom_av1_set_mb_ssim_rdmult_scaling(PictureParentControlSet* pcs) {
     Av1Common* cm       = pcs->av1_cm;
     const int  y_stride = pcs->enhanced_pic->y_stride;
@@ -2172,16 +2233,57 @@ static void aom_av1_set_mb_ssim_rdmult_scaling(PictureParentControlSet* pcs) {
             double    var = 0.0, num_of_var = 0.0;
             const int index = row * num_cols + col;
 
-            // Loop through each 8x8 block.
-            for (int mi_row = row * num_mi_h; mi_row < cm->mi_rows && mi_row < (row + 1) * num_mi_h; mi_row += 2) {
-                for (int mi_col = col * num_mi_w; mi_col < cm->mi_cols && mi_col < (col + 1) * num_mi_w; mi_col += 2) {
-                    const int row_offset_y = mi_row << 2;
-                    const int col_offset_y = mi_col << 2;
+            if (pcs->scs->static_config.alt_ssim_tuning) {
+                const int mi_row       = row << 2;
+                const int mi_col       = col << 2;
+                const int row_offset_y = row << 2;
+                const int col_offset_y = col << 2;
 
-                    const uint8_t* buf = y_buffer + row_offset_y * y_stride + col_offset_y;
+                // Loop through each 4x4 block within the 16x16 block.
+                for (int mi_row_3 = mi_row; mi_row_3 < cm->mi_rows && mi_row_3 < (row + 1) * num_mi_h; mi_row_3 += 1) {
+                    for (int mi_col_3 = mi_col; mi_col_3 < cm->mi_cols && mi_col_3 < (col + 1) * num_mi_w;
+                         mi_col_3 += 1) {
+                        const int row_offset_y_3 = mi_row_3 << 2;
+                        const int col_offset_y_3 = mi_col_3 << 2;
 
-                    var += svt_aom_get_perpixel_variance(buf, y_stride, BLOCK_8X8);
-                    num_of_var += 1.0;
+                        const uint8_t* buf3 = y_buffer + row_offset_y_3 * y_stride + col_offset_y_3;
+
+                        var += svt_aom_get_perceptual_perpixel_variance(buf3, y_stride, BLOCK_4X4);
+                        num_of_var += 0.015625; // Weigh at 1x weight (0.25 of total num_of_var)
+                    }
+                }
+                // Loop through each 8x8 block within the 16x16 block.
+                for (int mi_row_2 = mi_row; mi_row_2 < cm->mi_rows && mi_row_2 < (row + 1) * num_mi_h; mi_row_2 += 2) {
+                    for (int mi_col_2 = mi_col; mi_col_2 < cm->mi_cols && mi_col_2 < (col + 1) * num_mi_w;
+                         mi_col_2 += 2) {
+                        const int row_offset_y_2 = mi_row_2 << 2;
+                        const int col_offset_y_2 = mi_col_2 << 2;
+
+                        const uint8_t* buf2 = y_buffer + row_offset_y_2 * y_stride + col_offset_y_2;
+
+                        var += svt_aom_get_perceptual_perpixel_variance(buf2, y_stride, BLOCK_8X8);
+                        num_of_var +=
+                            0.125; // This weight (8x8 block) is 2x more important compared to other num_of_var additions
+                        // (0.5 of total num_of_var)
+                    }
+                }
+                const uint8_t* buf = y_buffer + row_offset_y * y_stride + col_offset_y;
+
+                var += svt_aom_get_perceptual_perpixel_variance(buf, y_stride, BLOCK_16X16);
+                num_of_var += 0.25; // Weigh at 1x weight (0.25 of total num_of_var)
+            } else {
+                // Loop through each 8x8 block.
+                for (int mi_row = row * num_mi_h; mi_row < cm->mi_rows && mi_row < (row + 1) * num_mi_h; mi_row += 2) {
+                    for (int mi_col = col * num_mi_w; mi_col < cm->mi_cols && mi_col < (col + 1) * num_mi_w;
+                         mi_col += 2) {
+                        const int row_offset_y = mi_row << 2;
+                        const int col_offset_y = mi_col << 2;
+
+                        const uint8_t* buf = y_buffer + row_offset_y * y_stride + col_offset_y;
+
+                        var += svt_aom_get_perpixel_variance(buf, y_stride, BLOCK_8X8);
+                        num_of_var += 1.0;
+                    }
                 }
             }
             var = var / num_of_var;
@@ -2206,20 +2308,52 @@ static void aom_av1_set_mb_ssim_rdmult_scaling(PictureParentControlSet* pcs) {
             }
         }
     }
-    log_sum = exp(log_sum / (double)(num_rows * num_cols));
-    if (do_print) {
-        fprintf(stderr, "\nlog_sum %.4f\n", log_sum);
-        fprintf(stderr, "16x16 block rdmult scaling factors");
-    }
-    for (int row = 0; row < num_rows; ++row) {
-        for (int col = 0; col < num_cols; ++col) {
-            const int index = row * num_cols + col;
-            pcs->pa_me_data->ssim_rdmult_scaling_factors[index] /= log_sum;
-            if (do_print) {
-                if (col == 0) {
-                    fprintf(stderr, "\n");
+    if (!pcs->scs->static_config.alt_ssim_tuning) {
+        log_sum = exp(log_sum / (double)(num_rows * num_cols));
+        if (do_print) {
+            fprintf(stderr, "\nlog_sum %.4f\n", log_sum);
+            fprintf(stderr, "16x16 block rdmult scaling factors");
+        }
+        for (int row = 0; row < num_rows; ++row) {
+            for (int col = 0; col < num_cols; ++col) {
+                const int index = row * num_cols + col;
+                pcs->pa_me_data->ssim_rdmult_scaling_factors[index] /= log_sum;
+                if (do_print) {
+                    if (col == 0) {
+                        fprintf(stderr, "\n");
+                    }
+                    fprintf(stderr, "%.4f\t", pcs->pa_me_data->ssim_rdmult_scaling_factors[index]);
                 }
-                fprintf(stderr, "%.4f\t", pcs->pa_me_data->ssim_rdmult_scaling_factors[index]);
+            }
+        }
+    } else { // Do superblock-based adjustment if we're using alternative SSIM tuning
+        const int sb_size     = pcs->scs->seq_header.sb_size;
+        const int num_mi_w_sb = mi_size_wide[sb_size];
+        const int num_mi_h_sb = mi_size_high[sb_size];
+        const int num_cols_sb = (cm->mi_cols + num_mi_w_sb - 1) / num_mi_w_sb;
+        const int num_rows_sb = (cm->mi_rows + num_mi_h_sb - 1) / num_mi_h_sb;
+        const int num_blk_w   = num_mi_w_sb / num_mi_w;
+        const int num_blk_h   = num_mi_h_sb / num_mi_h;
+        for (int row = 0; row < num_rows_sb; ++row) {
+            for (int col = 0; col < num_cols_sb; ++col) {
+                double log_sum_sb = 0.0;
+                double blk_count  = 0.0;
+                for (int blk_row = row * num_blk_h; blk_row < (row + 1) * num_blk_h && blk_row < num_rows; ++blk_row) {
+                    for (int blk_col = col * num_blk_w; blk_col < (col + 1) * num_blk_w && blk_col < num_cols;
+                         ++blk_col) {
+                        const int index = blk_row * num_cols + blk_col;
+                        log_sum_sb += log(pcs->pa_me_data->ssim_rdmult_scaling_factors[index]);
+                        blk_count += 1.0;
+                    }
+                }
+                log_sum_sb = exp(log_sum_sb / blk_count);
+                for (int blk_row = row * num_blk_h; blk_row < (row + 1) * num_blk_h && blk_row < num_rows; ++blk_row) {
+                    for (int blk_col = col * num_blk_w; blk_col < (col + 1) * num_blk_w && blk_col < num_cols;
+                         ++blk_col) {
+                        const int index = blk_row * num_cols + blk_col;
+                        pcs->pa_me_data->ssim_rdmult_scaling_factors[index] /= log_sum_sb;
+                    }
+                }
             }
         }
     }